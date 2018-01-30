00280d8695652711704f3425cf4b76b5a5865838
tnelsonw <nelsoccer77@yahoo.com>
2018-01-30 09:07:40 -0700

    Adding Server model classes.

---
+++ Server/src/main/java/com/example/server/Model/TicketToRideGame.java
@@ -0,0 +1,41 @@
+package com.example.tnels.tickettoride;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tnels on 1/29/2018.
+ */
+
+public class TicketToRideGame {
+
+    private List<Player> players;
+
+    private int gameID;
+
+    public TicketToRideGame(Player player)
+    {
+        players = new ArrayList<>();
+        players.add(player);
+
+    }
+
+    public TicketToRideGame(List<Player> players)
+    {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player)
+    {
+        if(players.size() < 5)
+            players.add(player);
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+}
