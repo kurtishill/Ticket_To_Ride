638431dc1199ed460f315b57f991273705f1e581
claytonk2 <35855013+claytonk2@users.noreply.github.com>
2018-02-02 18:02:02 -0700

    client facade

---
+++ app/src/main/java/AsyncTasks/RegisterTask.java
@@ -0,0 +1,38 @@
+package AsyncTasks;
+
+import android.os.AsyncTask;
+
+import com.example.server.Facade.RegisterService;
+import com.example.server.Results.RegisterResult;
+
+import client_facade.ClientFacade;
+import client_model.ClientModelRoot;
+
+/**
+ * Created by Clayton Kings on 2/2/2018.
+ */
+
+public class RegisterTask extends AsyncTask <String, Void, RegisterResult> {
+    @Override
+    protected RegisterResult doInBackground(String... strings){
+        ClientFacade facade =  new ClientFacade();
+        return facade.Register(strings[0], strings[1]);
+    }
+
+    @Override
+    protected void onPostExecute(RegisterResult registerResult) {
+        if(registerResult.isSuccess()){
+            ClientModelRoot.instance().setUser(registerResult.getAuthToken());
+        }
+        else {
+           /* CharSequence text = loginResult.getErrorMessage();
+
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+
+            toast.show();*/ // fixme do we want to show a toast here??
+        }
+        super.onPostExecute(registerResult);
+    }
+}
---
+++ app/src/main/java/PollerPack/Poller.java
@@ -0,0 +1,8 @@
+package PollerPack;
+
+/**
+ * Created by Clayton Kings on 2/2/2018.
+ */
+
+public class Poller {
+}
