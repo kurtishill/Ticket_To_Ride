5c10eceb746501dd81d5cb95e5f3b453a65dd56a
tnelsonw <nelsoccer77@yahoo.com>
2018-02-06 13:35:10 -0700

    Adding Async Tasks

---
+++ app/src/main/java/AsyncTasks/JoinGameTask.java
@@ -0,0 +1,14 @@
+package AsyncTasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by tnels on 2/6/2018.
+ */
+
+public class JoinGameTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
