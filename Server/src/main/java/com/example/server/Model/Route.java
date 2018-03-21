d2edb0eb01df07cbc4f9d02059a27856de7605c1
claytonk2 <35855013+claytonk2@users.noreply.github.com>
2018-03-21 13:51:26 -0600

    dest card calulator
    working destination calualtor

--- Server/src/main/java/com/example/server/Model/Route.java
+++ Server/src/main/java/com/example/server/Model/Route.java
@@ -12,7 +12,18 @@ public class Route
     private City city1;
     private City city2;
     private Player owner;
+    private boolean visited = false;
 
+    public boolean IsVisited(){
+        return visited;
+    }
+
+    public void Reset(){
+        visited = false;
+    }
+     public void Visit(){
+        visited = true;
+     }
     //call this constructor when initializing a game. Without player occupying route
     public Route(int length, int pointValue, String color, City city1, City city2)
     {
