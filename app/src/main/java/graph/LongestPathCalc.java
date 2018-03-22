1c25ffc0ced59142c04da8367bd02832f3493075
claytonk2 <35855013+claytonk2@users.noreply.github.com>
2018-03-22 12:20:08 -0600

    working longest paths
    works

--- app/src/main/java/graph/LongestPathCalc.java
+++ app/src/main/java/graph/LongestPathCalc.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 /**
  * Created by Clayton Kings on 3/21/2018.
+ * the only issue I see is when there is a loop and a branch off of that it sometimes counts it as a
+ * long line and doesnt just take the longest part. Only occurs ehen the loop begins at the longest path owned
  */
 
 public class LongestPathCalc {
@@ -19,96 +21,7 @@ public class LongestPathCalc {
         return false;
     }
     // could search multiple times and take the largest found.. this will probably be more accurate;
-    public int Search(List<Route> graph, int start, boolean begin){
-        City city1 = graph.get(start).getCity1();
-        City city2 = graph.get(start).getCity2();
-        int length = graph.get(start).getLength();
-        int side = 0;
-        boolean nothingAdded = true;
-        int tempGreatest = 0;
-        int index = 0;
-        graph.get(start).Visit();
-        for (int i = 0; i < graph.size(); i++) {
-            if (!graph.get(i).IsVisited()) {
-                if (graph.get(i).getCity1().equals(city1)) {
-                    if (graph.get(i).getLength() > tempGreatest) {
-                        tempGreatest = graph.get(i).getLength();
-                        index = i;
-                        nothingAdded = false;
-                        side = 1;
-                    }
-                }
-                else if (graph.get(i).getCity1().equals(city2)) {
-                    if (graph.get(i).getLength() > tempGreatest) {
-                        tempGreatest = graph.get(i).getLength();
-                        index = i;
-                        side = 2;
-                        nothingAdded = false;
-                    }
-                }
-                else if (graph.get(i).getCity2().equals(city1)) {
-                    if (graph.get(i).getLength() > tempGreatest) {
-                        tempGreatest = graph.get(i).getLength();
-                        index = i;
-                        side = 1;
-                        nothingAdded = false;
-                    }
-                }
-                else if (graph.get(i).getCity2().equals(city2)) {
-                    if (graph.get(i).getLength() > tempGreatest) {
-                        tempGreatest = graph.get(i).getLength();
-                        index = i;
-                        side = 2;
-                        nothingAdded = false;
-                    }
-                }
-            }
-        }
-        int indexSide = 0;
-        if(begin){
-            tempGreatest = 0;
-            for (int i = 0; i < graph.size(); i++) {
-                if (!graph.get(i).IsVisited()) {
-                    if (side == 1) {
-                        if (graph.get(i).getCity1().equals(city2)) {
-                            if (graph.get(i).getLength() > tempGreatest) {
-                                tempGreatest = graph.get(i).getLength();
-                                indexSide = i;
-                                nothingAdded = false;
-                            }
-                        }
-                        else if (graph.get(i).getCity2().equals(city2)) {
-                            if (graph.get(i).getLength() > tempGreatest) {
-                                tempGreatest = graph.get(i).getLength();
-                                indexSide = i;
-                                nothingAdded = false;
-                            }
-                        }
-                    }
-                    else if(side ==2){
-                        if (graph.get(i).getCity1().equals(city1)) {
-                            if (graph.get(i).getLength() > tempGreatest) {
-                                tempGreatest = graph.get(i).getLength();
-                                indexSide = i;
-                                nothingAdded = false;
-                            }
-                        }
-                        else if (graph.get(i).getCity2().equals(city1)) {
-                            if (graph.get(i).getLength() > tempGreatest) {
-                                tempGreatest = graph.get(i).getLength();
-                                indexSide = i;
-                                nothingAdded = false;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if (nothingAdded){
-            return 0;
-        }
-        return length + Search(graph, index, false) + Search(graph, indexSide, false);
-    }
+
     public int LongestPath(List<Route> graph){
         int tempLargest = 0;
         int largest = 0;
--- app/src/test/java/graph/LongestPathCalcTest.java
+++ app/src/test/java/graph/LongestPathCalcTest.java
@@ -36,5 +36,23 @@ public class LongestPathCalcTest {
         int longestPath = calc.LongestPath(graph);
         assertEquals(longestPath, 16);
     }
+    @Test
+    public void longestPathWBranches() throws Exception {
+        List<Route> graph = new ArrayList<>();
+        LongestPathCalc calc = new LongestPathCalc();
+        graph.add(allRoutes.get(1)); // 3 // shorter branch
+        graph.add(allRoutes.get(4)); // 5 // shorter branch
+        graph.add(allRoutes.get(6)); // 6
+        graph.add(allRoutes.get(8)); // 1
+        graph.add(allRoutes.get(10)); // 1
+        graph.add(allRoutes.get(19)); // 3 longer branch
+        graph.add(allRoutes.get(85)); // 5 longer branch
+        graph.add(allRoutes.get(82)); // 2 longer branch
+        graph.add(allRoutes.get(86)); // 4 longer branch
+        graph.add(allRoutes.get(21));// 5
+        graph.add(allRoutes.get(28)); // 4
+        int longestPath = calc.LongestPath(graph);
+        assertEquals(longestPath, 22);
+    }
 
 }
