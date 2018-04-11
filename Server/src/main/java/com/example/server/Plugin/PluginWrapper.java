31e3cfee2502cc8fe72b6f8a05792780d8a149be
Clayton Kingsbury <ckingsbury2@gmail.com>
2018-04-11 16:28:49 -0600

    Ipluginwrapper
    plugin wrappin

---
+++ Server/src/main/java/com/example/server/Plugin/PluginWrapper.java
@@ -0,0 +1,17 @@
+package com.example.server.Plugin;
+
+import com.example.server.Database.StoredData;
+
+/**
+ * Created by claytonkingsbury on 4/11/18.
+ */
+
+public class PluginWrapper {
+    public IPlugin plugin;
+    private static PluginWrapper _instance;
+    public static PluginWrapper instance() {
+        if(_instance == null)
+            _instance = new PluginWrapper();
+        return _instance;
+    }
+}
