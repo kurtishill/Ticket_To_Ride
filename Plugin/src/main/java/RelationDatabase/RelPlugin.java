1f801b750c46d2728e9edc91343f26e1e07461a3
Clayton Kingsbury <ckingsbury2@gmail.com>
2018-04-11 21:26:31 -0600

    start of rel plugin
    start rel plugin

---
+++ Plugin/src/main/java/RelationDatabase/RelPlugin.java
@@ -0,0 +1,40 @@
+package RelationDatabase;
+
+import Plugin.ICommandDao;
+import Plugin.IGameDao;
+import Plugin.IPlugin;
+import Plugin.IUserDao;
+
+/**
+ * Created by claytonkingsbury on 4/11/18.
+ */
+
+public class RelPlugin implements IPlugin {
+    IGameDao gameDao;
+    IUserDao userDao;
+    ICommandDao commandDao;
+    public RelPlugin(){
+        gameDao = new RelGameDao();
+        userDao = new RelUserDao();
+        commandDao = new RelCommandDao();
+    }
+    @Override
+    public IPlugin instance() {
+        return this;
+    }
+
+    @Override
+    public IGameDao getGameDao() {
+        return gameDao;
+    }
+
+    @Override
+    public IUserDao getUserDao() {
+        return userDao;
+    }
+
+    @Override
+    public ICommandDao getCommandDao() {
+        return commandDao;
+    }
+}
